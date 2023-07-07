4398f0690544e2d2d7c5a23590f0735854b00c06
Yan Valadares <yancardoso@outlook.com.br>
2023-07-07 18:31:47 -0300

    Adicionando cookies a aplicação

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,23 +2,32 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
-// import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const meals = await knex('meals').select()
+  app.get(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+      const meals = await knex('meals')
+        .select()
+        .where({ session_id: sessionId })
 
-    return { meals }
-  })
+      return { meals }
+    },
+  )
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
     const getMealParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getMealParamsSchema.parse(request.params)
 
-    const meal = await knex('meals').select().where('id', id).first()
+    const sessionId = request.cookies.sessionId
+
+    const meal = await knex('meals').select().first()
 
     return { meal }
   })
@@ -34,11 +43,23 @@ export async function mealsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('meals').insert({
       id: randomUUID(),
       name,
       description,
       healthy: healthy === 'yes',
+      session_id: sessionId,
     })
 
     return reply.status(201).send

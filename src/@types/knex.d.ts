0bc727cb713840c3048d63d926b33a0fa097f4d5
Yan Valadares <yancardoso@outlook.com.br>
2023-07-07 18:08:16 -0300

    Criação de rotas de listagem de refeições

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    meals: {
+      id: string
+      name: string
+      description: string
+      meal_time: string
+      healthy: Boolean
+      session_id?: string
+    }
+  }
+}
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -5,6 +5,24 @@ import { knex } from '../database'
 // import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const meals = await knex('meals').select()
+
+    return { meals }
+  })
+
+  app.get('/:id', async (request) => {
+    const getMealParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getMealParamsSchema.parse(request.params)
+
+    const meal = await knex('meals').select().where('id', id).first()
+
+    return { meal }
+  })
+
   app.post('/', async (request, reply) => {
     const createMealBodySchema = z.object({
       name: z.string(),
